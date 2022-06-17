92a4aa06e62adb0eebcc1d4916ba48349c6bce25
ygeras <86482316+Yus312@users.noreply.github.com>
2022-06-17 12:42:30 +0600

    Initial Commit

---
+++ Friendface/FriendfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendfaceApp.swift
+//  Friendface
+//
+//  Created by Yuri Gerasimchuk on 17.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct FriendfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
